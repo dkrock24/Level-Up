b30757df2812382155212b0889d5ae8b6829b0ba
Rafael Gutierrez <Rafael.Gutierrez@Focusservices.com>
2018-01-22 11:12:56 -0600

    sorce

---
+++ src/main/java/com/focus/levelup/LevelUpApplication.java
@@ -0,0 +1,12 @@
+package com.focus.levelup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelUpApplication.class, args);
+	}
+}
