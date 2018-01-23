fed3be20adcd76d7c9cf4d84d95835bb81e65a4b
Rafael Gutierrez <Rafael.Gutierrez@Focusservices.com>
2018-01-23 08:29:48 -0600

    Added Template

--- src/main/java/com/focus/levelup/controller/IndexController.java
+++ src/main/java/com/focus/levelup/controller/IndexController.java
@@ -1,16 +1,31 @@
 package com.focus.levelup.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.focus.levelup.entity.Role;
+import com.focus.levelup.services.RoleServices;
+
 @Controller
 public class IndexController {
 
+	@Autowired
+	RoleServices roleServices;
+	
 	@RequestMapping("/index")
 	public String demo(Model model) {
 		
-		model.addAttribute("data", "Welcome");
+		List<Role> role =  (List<Role>) roleServices.findAll();
+		
+		for(Role roles : role) {
+			System.out.println(roles.getRole());
+		}
+		
+		model.addAttribute("data", role);
 		return "home/index";
 	}
 }
