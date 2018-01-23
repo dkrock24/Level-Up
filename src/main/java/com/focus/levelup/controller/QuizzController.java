88d0a4ac120e0350d15ba0de99b0bee58cf3225b
Rafael Gutierrez <Rafael.Gutierrez@Focusservices.com>
2018-01-23 16:02:04 -0600

    Crud Usuarios y languages

---
+++ src/main/java/com/focus/levelup/controller/QuizzController.java
@@ -0,0 +1,88 @@
+package com.focus.levelup.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.focus.levelup.model.ProgrammingLanguage;
+import com.focus.levelup.model.Users;
+import com.focus.levelup.services.ProgrammingLanguageService;
+import com.focus.levelup.services.QuizLevelsService;
+import com.focus.levelup.services.QuizzesServices;
+import com.focus.levelup.services.TestsService;
+
+
+
+@Controller
+@RequestMapping("Quizz")
+public class QuizzController {
+	
+	@Autowired
+	ProgrammingLanguageService languagesServices;
+	
+	@Autowired
+	QuizLevelsService QlevelServices;
+	
+	@Autowired
+	QuizzesServices quizzesServices;
+	
+	@Autowired
+	TestsService testServices;
+
+	@RequestMapping("index")
+	public String demo(Model model) {
+		
+		int countLanguages = (int) languagesServices.count();
+		int countQLevel = (int) QlevelServices.count();
+		int countQuizz = (int) quizzesServices.count();
+		int countPendingTest = (int) testServices.count();
+				
+		model.addAttribute("totalLanguages", countLanguages);
+		model.addAttribute("totalLevels", countQLevel);
+		model.addAttribute("countQuizz", countQuizz);
+		model.addAttribute("countPendingTest", countPendingTest);
+		
+		return "quizz/index";
+	}
+	
+	@RequestMapping("addLanguages")
+	public String addLanguages(Model model) {
+		
+		List<ProgrammingLanguage> pl = (List<ProgrammingLanguage>) languagesServices.findAll();
+		
+		model.addAttribute("pl",pl);
+		
+		// Basic Count 
+		int countLanguages = (int) languagesServices.count();
+		int countQLevel = (int) QlevelServices.count();
+		int countQuizz = (int) quizzesServices.count();
+		int countPendingTest = (int) testServices.count();
+				
+		model.addAttribute("totalLanguages", countLanguages);
+		model.addAttribute("totalLevels", countQLevel);
+		model.addAttribute("countQuizz", countQuizz);
+		model.addAttribute("countPendingTest", countPendingTest);
+		// End Basic Count
+		
+		return "quizz/addLanguages";
+	}
+	
+	@RequestMapping("saveLanguage")
+	public String saveLanguage(@ModelAttribute("ProgrammingLanguage") ProgrammingLanguage languages, BindingResult result) {
+		
+		ProgrammingLanguage pl = new ProgrammingLanguage();
+		pl.setLanguage(languages.getLanguage());
+		pl.setStatus(languages.getStatus());
+		
+		languagesServices.save(pl);
+		
+		return "quizz/addLanguages";
+	}
+	
+	
+}
--- src/main/java/com/focus/levelup/controller/UsersController.java
+++ src/main/java/com/focus/levelup/controller/UsersController.java
@@ -5,12 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.focus.levelup.model.Roles;
 import com.focus.levelup.model.Users;
+import com.focus.levelup.services.RoleServices;
 import com.focus.levelup.services.UserService;
 
 @Controller
@@ -19,6 +23,9 @@ public class UsersController {
 
 	@Autowired
 	UserService usersServices;
+	
+	@Autowired
+	RoleServices roleServices;
 	
 	@RequestMapping("index")
 	public String users(Model model) {
@@ -33,10 +40,31 @@ public class UsersController {
 	@RequestMapping(value= "edit/{id}", method = RequestMethod.GET)
 	public String edit(Model model, @PathVariable int id) {
 		
-		Users user =  usersServices.findOne(id);	
+		Users user =  usersServices.findOne(id);
+		
+		List<Roles> role =  (List<Roles>) roleServices.findAll();
 		
 		model.addAttribute("users", user);
+		model.addAttribute("roles", role);
 		
 		return "users/update";
 	}
+	
+	@RequestMapping(value= "saveUpdate", method = RequestMethod.POST)
+	public ModelAndView saveUpdate(@ModelAttribute("Users") Users users, BindingResult result ) {
+		
+		Users user =  usersServices.findOne(users.getIdUser());
+		
+		user.setRole(users.getRole());		
+		user.setFirstName(users.getFirstName());
+		user.setLastName(users.getLastName());
+		user.setEmail(users.getEmail());
+		user.setStatus(users.getStatus());
+		
+		usersServices.save( user );
+		
+		return new ModelAndView("redirect:/Users/index");
+	}
+	
+	
 }
