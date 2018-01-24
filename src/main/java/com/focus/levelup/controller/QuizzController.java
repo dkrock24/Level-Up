757dda052b2f51aab02d709af4161b318d2203fe
Rafael Gutierrez <Rafael.Gutierrez@Focusservices.com>
2018-01-24 16:50:41 -0600

    Add Quizz and Questions

--- src/main/java/com/focus/levelup/controller/QuizzController.java
+++ src/main/java/com/focus/levelup/controller/QuizzController.java
@@ -14,10 +14,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.focus.levelup.model.ProgrammingLanguage;
+import com.focus.levelup.model.QuestionTypes;
+import com.focus.levelup.model.Questions;
 import com.focus.levelup.model.QuizLevels;
 import com.focus.levelup.model.Quizzes;
 import com.focus.levelup.model.Users;
 import com.focus.levelup.services.ProgrammingLanguageService;
+import com.focus.levelup.services.QuestionTypesService;
 import com.focus.levelup.services.QuizLevelsService;
 import com.focus.levelup.services.QuizzesServices;
 import com.focus.levelup.services.TestsService;
@@ -43,6 +46,9 @@ public class QuizzController {
 	
 	@Autowired
 	UserService userServices;
+	
+	@Autowired
+	QuestionTypesService questionTypeServices;
 
 	@RequestMapping("index")
 	public String index(Model model) {
@@ -168,7 +174,8 @@ public class QuizzController {
 		
 		//Find user
 		Users user = userServices.findOne(2);
-		Date d = new Date();
+		QuizLevels ql = QlevelServices.findOne(1);
+		Date d = new Date();		
 		
 		//Create Instance of Quizz
 		Quizzes quizzes = new Quizzes();
@@ -177,11 +184,90 @@ public class QuizzController {
 		quizzes.setDescription(quizz.getDescription());
 		quizzes.setCreatedOn(d);
 		quizzes.setCreatedBy("Test");
+		quizzes.setStatus(quizz.getStatus());
 		quizzes.setUser(user);		
+		
+		Quizzes quiz = quizzesServices.save(quizzes);		
+		int idQuiz = quiz.getIdQuiz();
+		
+		return new ModelAndView("redirect:/Quizz/addQuestion/"+idQuiz);
+	}
+	
+	@RequestMapping(value ="addQuestion/{idQuiz}")
+	public String addQuestion(Model model,@PathVariable int idQuiz) {
+		
+		// GET Quizz
+		Quizzes quiz = quizzesServices.findOne(idQuiz);	
+		model.addAttribute("quizz", quiz);
+		
+		List<Questions> question = (List<Questions>) quiz.getQuestions();
+		for(Questions q :question) {
+			System.out.println(q.getQuestion());
+		}
+		
+		// GET Question
+		model.addAttribute("question",question);
+		
+		List<Quizzes> quizzes = (List<Quizzes>) quizzesServices.findAll();		
+		
+		int countLanguages = (int) languagesServices.count();
+		int countQLevel = (int) QlevelServices.count();
+		int countQuizz = (int) quizzesServices.count();
+		int countPendingTest = (int) testServices.count();
+				
+		model.addAttribute("totalLanguages", countLanguages);
+		model.addAttribute("totalLevels", countQLevel);
+		model.addAttribute("countQuizz", countQuizz);
+		model.addAttribute("countPendingTest", countPendingTest);
+		
+		model.addAttribute("quizzes",quizzes);
+		
+		// Services		
+		List<QuestionTypes> questionTypes = (List<QuestionTypes>) questionTypeServices.findAll();
+		List<QuizLevels> ql = (List<QuizLevels>) QlevelServices.findAll();
+		
+		model.addAttribute("questionTypes", questionTypes);
+		model.addAttribute("ql",ql);
+		
+		return ("quizz/addQuestion");
+	}
+	
+	@RequestMapping(value="editQuizz/{id}", method= RequestMethod.GET )
+	public String editQuizz(Model model, @PathVariable int id) {
+		
+		List<ProgrammingLanguage> pl = (List<ProgrammingLanguage>) languagesServices.findAll();	
+		List<QuizLevels> ql = (List<QuizLevels>) QlevelServices.findAll();		
+		Quizzes quizz = quizzesServices.findOne(id);
+		
+		model.addAttribute("languages",pl);
+		model.addAttribute("ql",ql);		
+		model.addAttribute("quizz", quizz);
+		
+		return "quizz/editQuizz";
+	}
+	
+	@RequestMapping(value ="updateQuizz")
+	public ModelAndView updateQuizz(@ModelAttribute("Quizzes") Quizzes quizz, BindingResult result) {
+		
+		//Find Quizz
+		Quizzes quizzes = quizzesServices.findOne(quizz.getIdQuiz());
+		
+		Date d = new Date();		
+		
+		//Create Instance of Quizz
+		quizzes.setProgrammingLanguage(quizz.getProgrammingLanguage());
+		quizzes.setQuizLevel(quizz.getQuizLevel());
+		quizzes.setDescription(quizz.getDescription());
+		quizzes.setUpdatedOn(d);
+		quizzes.setCreatedBy("Test");
+		quizzes.setStatus(quizz.getStatus());
+		quizzes.setUser(quizz.getUser());		
 		
 		quizzesServices.save(quizzes);
 		
 		return new ModelAndView("redirect:/Quizz/index");
 	}
+	
+	
 	
 }
